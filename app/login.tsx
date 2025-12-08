54c0093d4f2a8e5df2787c28774f5bad7e30972c
Shreya <shreyagrg888@gmail.com>
2025-12-08 19:47:49 +0545

    corrected the api of login

--- app/login.tsx
+++ app/login.tsx
@@ -24,7 +24,7 @@ export default function Login() {
     try {
       setLoading(true);
 
-      const response = await fetch("http://192.168.1.76/api/auth/login", {
+      const response = await fetch("http://localhost:5000/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
