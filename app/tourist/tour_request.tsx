d790dea3af1112495d9239220ba89d69d4690c76
Shreya <shreyagrg888@gmail.com>
2026-02-15 07:40:11 +0545

    renamed search to tour_request

--- app/tourist/tour_request.tsx
+++ app/tourist/tour_request.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function search() {
+export default function tour_request() {
   return (
     <View>
-      <Text>search</Text>
+      <Text>tour_request</Text>
     </View>
   )
 }
